4037670dd01c21b8ee2e936e28d9d37528998c95
Willem Röpke <willemropke@hotmail.com>
2019-04-10 11:49:33 +0200

    Changed the importer so that it returns 3 CSV files containing the different splits instead of 1 big file with all the data

--- import_cgn.py
+++ import_cgn.py
@@ -8,10 +8,17 @@ import pandas as pd     # Pandas is used to construct the CSV file
 import xml.etree.ElementTree as ET      # This is used to parse the XML file with the transcriptions
 import gzip          # This is used to extract the transcriptions if they are still zipped
 
-# Global variable specifying the maximum accepted length of a speech file
+# Global variable specifying the maximum and minimum accepted lengths of a speech file
 MAX_SECS = 10
 MIN_SECS = 1
-FILENAME_ALL = "all_data.csv"
+
+# The filenames for the splits
+FILENAME_TRAIN = "train_data.csv"
+FILENAME_DEV = "dev_data.csv"
+FILENAME_TEST = "test_data.csv"
+
+# The percentage by which we split the training and testing sets
+TRAIN_SPLIT = 0.8
 
 
 # This function will preprocess the data given a target (the top level directory of CGN)
@@ -31,6 +38,9 @@ def preprocess_data(args):
     audio_path = path.join(target, "data/audio/wav")
     trans_path = path.join(target, "data/annot/xml/skp-ort")
 
+    # The dataframe holding all the data
+    data = pd.DataFrame()
+
     # Check if the user chose a specific component
     if args.component:
         audio_path = path.join(audio_path, "comp-" + args.component)
@@ -40,7 +50,8 @@ def preprocess_data(args):
         else:
             print("---------------------------------------------------------")
             print("Only utilizing the specified component " + args.component)
-            process_component(audio_path, trans_path)
+            new_data = process_component(audio_path, trans_path)
+            data = data.append(new_data)
             print("---------------------------------------------------------")
     else:
         print("Utilizing all available components")
@@ -52,13 +63,19 @@ def preprocess_data(args):
                 new_trans_path = path.join(trans_path, x)
                 print("---------------------------------------------------------")
                 print("Entering directory " + x)
-                process_component(new_audio_path, new_trans_path)
+                new_data = process_component(new_audio_path, new_trans_path)
+                data = data.append(new_data)
                 print("---------------------------------------------------------")
+
+    # Generate the final splits
+    generate_splits(data)
 
 
 # This function takes care of one component of the data
 def process_component(audio_path, trans_path):
     lang = args.language
+
+    data = pd.DataFrame()
     # If flemish is selected we will enter this
     # If no language was chose we want both so we also enter this
     if not lang or lang == "vl":
@@ -67,7 +84,8 @@ def process_component(audio_path, trans_path):
         new_trans_path = path.join(trans_path, "vl")
         # This check is just to make sure the directory exists
         if os.path.isdir(new_audio_path):
-            process_language(new_audio_path, new_trans_path)
+            new_data = process_language(new_audio_path, new_trans_path)
+            data = data.append(new_data)
         else:
             print("Directory does not exist")
 
@@ -77,12 +95,15 @@ def process_component(audio_path, trans_path):
         new_audio_path = path.join(audio_path, "nl")
         new_trans_path = path.join(trans_path, "nl")
         if os.path.isdir(new_audio_path):
-            process_language(new_audio_path, new_trans_path)
+            new_data = process_language(new_audio_path, new_trans_path)
+            data = data.append(new_data)
         else:
             print("Directory does not exist")
 
     if lang and lang != "nl" and lang != "vl":
         print("The provided language was invalid")
+
+    return data
 
 
 # This function processes one language each time it gets called
@@ -109,7 +130,7 @@ def process_language(audio_path, trans_path):
         elif seconds > MAX_SECS or seconds < MIN_SECS:
             rejected += 1
         else:
-            accepted +=1
+            accepted += 1
             accepted_wavs.append(final_path)
             file_size = os.path.getsize(final_path)
             accepted_wav_sizes.append(file_size)
@@ -125,13 +146,10 @@ def process_language(audio_path, trans_path):
     # A data frame of all the processed data
     df = pd.DataFrame(processed_data, columns=['wav_filename', 'wav_filesize', 'transcript'])
 
-    # We write the data to the file. It is opened with "append" so that each component appends the previous one
-    with open(FILENAME_ALL, 'a') as f:
-        # We only want to write the header the first time, hence the quick check "header = ..."
-        df.to_csv(f, sep=',', mode='a', header=f.tell() == 0, index=False, encoding="ascii")
-
     print("Number of rejected files: " + str(rejected))
     print("Number of accepted files: " + str(accepted))
+
+    return df
 
 
 # This function generates the transcription for the given audio file
@@ -169,6 +187,44 @@ def get_transcription(audio_file, directory_path):
     return transcription
 
 
+def generate_splits(data):
+    print("Generating splits for the data")
+
+    # Randomize the order of the data
+    data.sample(frac=1)
+
+    # We split into 80-20 training and testing (using the global variable TRAIN_SPLIT)
+    # Afterwards we split the 80% training data into 80-20 training validation
+    train_beg = 0
+    train_end = int(TRAIN_SPLIT * len(data))
+    dev_beg = int(TRAIN_SPLIT * train_end)
+    dev_end = train_end
+    train_end = dev_beg
+    test_beg = dev_end
+    test_end = len(data)
+
+    # Make the splits
+    train_data = data[train_beg:train_end]
+    dev_data = data[dev_beg:dev_end]
+    test_data = data[test_beg:test_end]
+
+    # We write the splits
+    print("Writing training split")
+    with open(FILENAME_TRAIN, 'a') as f:
+        # We only want to write the header the first time, hence the quick check "header = ..."
+        train_data.to_csv(f, sep=',', mode='a', header=True, index=False, encoding="ascii")
+
+    print("Writing validation split")
+    with open(FILENAME_DEV, 'a') as f:
+        # We only want to write the header the first time, hence the quick check "header = ..."
+        dev_data.to_csv(f, sep=',', mode='a', header=True, index=False, encoding="ascii")
+
+    print("Writing test split")
+    with open(FILENAME_TEST, 'a') as f:
+        # We only want to write the header the first time, hence the quick check "header = ..."
+        test_data.to_csv(f, sep=',', mode='a', header=True, index=False, encoding="ascii")
+
+
 if __name__ == "__main__":
     print("Starting the preprocessing of the data")
 
@@ -179,9 +235,13 @@ if __name__ == "__main__":
     parser.add_argument("--language", help="choose a single language for the model")
     args = parser.parse_args()
 
-    # Getting rid of the previous CSV file if it exists
-    if os.path.exists(FILENAME_ALL):
-        os.remove(FILENAME_ALL)
+    # Getting rid of the previous CSV files if they exist
+    if os.path.exists(FILENAME_TRAIN):
+        os.remove(FILENAME_TRAIN)
+    if os.path.exists(FILENAME_DEV):
+        os.remove(FILENAME_DEV)
+    if os.path.exists(FILENAME_TEST):
+        os.remove(FILENAME_TEST)
 
     preprocess_data(args)
 
