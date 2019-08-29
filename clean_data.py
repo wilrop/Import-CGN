6af2629461d2d1843a1fd7193dea05cfac62a641
Willem Röpke <willem.ropke@vub.be>
2019-08-29 11:13:13 +0200

    Python script to clean a file from certain characters

---
+++ clean_data.py
@@ -0,0 +1,38 @@
+import argparse
+import pandas as pd
+from os import path
+
+
+def clean(args):
+    file = args.file
+    df = pd.read_csv(file)
+    print("There are " + len(df) + " in the original file")
+
+    # Deleting rows where the transcript contains the character &
+    df = df[df['transcript'].find("&") == -1]
+
+    for index, row in df.iterrows():
+        row['transcript'].replace("-", " ")
+
+    print("There are " + len(df) + " in the cleaned file")
+
+    # We write the cleaned file
+    print("Writing cleaned file")
+    new_file = path.dirname(file) + "/cleaned_" + path.basename(file)
+    with open(new_file, 'a') as f:
+        # We only want to write the header the first time, hence the quick check "header = ..."
+        df.to_csv(f, sep=',', index=False, encoding="ascii")
+
+
+# The main function
+if __name__ == "__main__":
+    print("Starting the cleaning of the data")
+
+    # Starting the parser for the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="the file that will be cleaned")
+    args = parser.parse_args()
+
+    clean(args)
+
+    print("Completed successfully")
